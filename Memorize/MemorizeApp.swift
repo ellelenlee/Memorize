b39d415cc519d15490ce64ffdbb364f8fee948be
Ellen <lixz051@icloud.com>
2024-03-26 21:14:43 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ellen Lee on 2024/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
